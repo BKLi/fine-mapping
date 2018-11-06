47d64cc5e395c7c78cd7bd65c3910fbc4a284f93
BKLi <libingkun1997@gmail.com>
2018-11-06 11:41:53 -0800

    add header to arg

--- SSTtoLDblock.py
+++ SSTtoLDblock.py
@@ -7,25 +7,29 @@ from collections import OrderedDict
 import sys
 
 
-def SSTtoLDblock(sstFile, distance, ldFile, prefix):
+def SSTtoLDblock(sstFile, header, distance, ldFile, prefix):
     # input SST should be lifted-over and split by chromosome first
     # forms of SST varies; change import step accordingly
 
+    header_list = header.split(" ")
     ld = pd.read_table(ldFile, delim_whitespace=True)
     sst = pd.read_table(sstFile, header=None, sep="\t",
-                        names=["CHR", "BP", "SNP", "A1", "A2", "FRQ_A", "FRQ_U", "INFO", "OR", "SE", "P", "report"])
+                        names=header_list)
 
     if sst.shape[0] == 0:
         print("empty file")
-        sys.exit()
+        sys.exit(0)
 
     group_ids = (sst["BP"] > (sst["BP"].shift() + distance)).cumsum()
     grouped_sst = sst.groupby(group_ids)
 
     locus_sizes = []
     for k, sub_sst in grouped_sst:
-        outfile = "{}_block_{}".format(prefix, str(k + 1))
-        file = open(outfile, "w+")
+
+        # print(sub_sst.shape[0])
+
+        outfile = "{}_block_{}.sst".format(prefix, str(k + 1))
+        # file = open(outfile, "w+")
         ID = sub_sst["BP"].tolist()
         sub_id = []  # list to store ID of LD-buddies
 
@@ -39,8 +43,13 @@ def SSTtoLDblock(sstFile, distance, ldFile, prefix):
         uniq_sub_id = list(OrderedDict.fromkeys(sub_id))
         uniq_sorted_sub_id = sorted(uniq_sub_id)
 
-        for uid in uniq_sorted_sub_id:
-            file.write("{}\n".format(uid))
+        # intersect each LD block with sst
+        uid_df = pd.DataFrame({"BP": uniq_sorted_sub_id})
+        merged = pd.merge(uid_df, sst, how='inner', on=["BP"])
+        print(merged.head())
+        # print(merged.shape[0])
+        # file.write("{}\n".format(uid))
+        merged.to_csv(outfile, sep="\t", index=False)
 
         # get length of each locus
         locus_sizes.append(sub_sst["BP"].max() - sub_sst["BP"].min())
@@ -49,6 +58,6 @@ def SSTtoLDblock(sstFile, distance, ldFile, prefix):
     print("average: ", mean(locus_sizes))
 
 
-# SSTtoLDblock("C:\\Users\libin\Desktop\MDD_chr1_filtered.sst", 1000000,
-# "C:\\Users\libin\Desktop\chr1_europe_0.2_1000000.txt", "chr1")
-SSTtoLDblock(sstFile=sys.argv[1], distance=int(sys.argv[2]), ldFile=sys.argv[3], prefix=sys.argv[4])
+SSTtoLDblock("C:\\Users\\libin\\Desktop\\tmp\\MDD_chr1_filtered.sst", "CHR BP SNP A1 A2 FRQ_A FRQ_U INFO OR SE P",
+             1000000, "C:\\Users\\libin\\Desktop\\tmp\\chr1_europe_0.2_1000000.txt", "chr1")
+# SSTtoLDblock(sstFile=sys.argv[1], distance=int(sys.argv[2]), ldFile=sys.argv[3], prefix=sys.argv[4])
